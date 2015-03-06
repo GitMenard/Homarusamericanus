68ef737c6e6ea70a8d4414f95809c08b69fa07d9
GitMenard <mmenard@oneworldonestop.com>
2015-03-06 10:29:23 -0500

    Learning Hour V1

--- Homarusamericanus/Scripts/Angular4.js
+++ Homarusamericanus/Scripts/Angular4.js
@@ -3,9 +3,20 @@
 
 
 //Create Routes
-angular.module('firstModule', []).config(function ($routeProvider) {
-    $routeProvider.
-        when('/Angular4', { controller: customersController, templateUrl: 'temp1.html' }).
-        when("/second", { controller: "SimpleController", templateUrl: "temp2.html" }).
-        otherwise({ redirectTo: "/first" });
+//////angular.module('firstModule', []).config(function ($routeProvider) {
+//////    $routeProvider.
+//////        when('/Angular4', { controller: customersController, templateUrl: 'temp1.html' }).
+//////        when("/second", { controller: "SimpleController", templateUrl: "temp2.html" }).
+//////        otherwise({ redirectTo: "/first" });
+//////});
+angular.module('app', [
+  'ngStorage'
+]).
+
+controller('Ctrl', function (
+  $scope,
+  $localStorage
+) {
+    $scope.$storage = $localStorage.$default({
+    });
 });

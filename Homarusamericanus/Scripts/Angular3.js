8198eb0f809806a53bfac764f6adc93277298a7d
GitMenard <mmenard@oneworldonestop.com>
2015-03-05 23:28:26 -0500

    Look over again

---
+++ Homarusamericanus/Scripts/Angular3.js
@@ -0,0 +1,6 @@
+﻿//@ sourceURL=Angular3.js
+function customersController($scope, $http) {
+    $http.get("http://www.w3schools.com/website/Customers_JSON.php")
+    .success(function (response) { $scope.names = response; });
+    var stop = 1;
+}
---
+++ Homarusamericanus/Scripts/Angular4.js
@@ -0,0 +1,11 @@
+﻿//@ sourceURL=Angular4.js
+//angular.module('firstModule', [])
+
+
+//Create Routes
+angular.module('firstModule', []).config(function ($routeProvider) {
+    $routeProvider.
+        when('/Angular4', { controller: customersController, templateUrl: 'temp1.html' }).
+        when("/second", { controller: "SimpleController", templateUrl: "temp2.html" }).
+        otherwise({ redirectTo: "/first" });
+});
